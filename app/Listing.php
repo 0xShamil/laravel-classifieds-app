86a4fc04887dd8edbf9d44cb64ebff70ac8bbae9
Shamil Choudhury <mail.shamilchoudhury@gmail.com>
2017-02-24 11:43:15 +0530

    Listings set up

---
+++ app/Listing.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\Traits\Eloquent\OrderableTrait;
+
+use App\{User, Category, Area};
+
+class Listing extends Model
+{
+    use SoftDeletes, OrderableTrait;
+
+    public function scopeIsLive($query)
+    {
+    	return $query->where('live', true);
+    }
+
+    public function scopeIsNotLive($query)
+    {
+    	return $query->where('live', false);
+    }
+
+    public function scopeFromCategory($query, Category $category)
+    {
+    	return $query->where('category_id', $category->id);
+    }
+
+    public function scopeInArea($query, Area $area)
+    {
+    	return $query->whereIn('area_id', array_merge(
+    		[$area->id],
+    		$area->descendants->pluck('id')->toArray()
+    	));
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+    	return $this->belongsTo(Category::class);
+    }
+
+    public function area()
+    {
+    	return $this->belongsTo(Area::class);
+    }
+
+    public function live()
+    {
+    	return $this->live;
+    }
+
+    public function cost()
+    {
+    	return $this->category->price;
+    }
+}
---
+++ database/migrations/2017_02_24_054558_create_listings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->integer('user_id')->unsigned();
+            $table->integer('area_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->boolean('live')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+}
